b2a18fc870b9873532a8f8c4c4643fe1b4e1d5e1
MCucca3 <93353956+MCucca3@users.noreply.github.com>
2021-10-28 19:47:55 -0300

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,8 @@
+function abrirCerrarMenu(){
+        var x = document.getElementsByClassName("menu")[0];
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+}
